6581ee401d25110cc8bf33de69690e32b7c76a4a
PxlFrey <fred.chretien@outlook.fr>
2022-10-19 23:02:59 +0200

    Added animations + slider + fixed some bugs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
-$(function() {
 
-  // NAVIGATION MOBILE
+
+// GESTION DU HEADER AVEC LE SCROLL
+
+const navbar = document.querySelector('#navbar');
+const headerLogo = document.querySelector('.header-logo');
+const headerLogoBlack = document.querySelector('.header-logo-black');
+const headerBtn = document.querySelector('.header-btn');
+const menuBlack = document.querySelector('.menu-black');
+
+window.addEventListener('scroll', () => {
+  
+  if (window.scrollY > 780)  {
+
+      navbar.classList.add('scroll');
+      headerLogo.classList.add('none');
+      menuBlack.classList.add('active');
+      headerLogoBlack.classList.add('active');
+      headerBtn.classList.add('active');
+
+
+     } else { 
+
+      navbar.classList.remove('scroll');
+      headerLogo.classList.remove('none');
+      menuBlack.classList.remove('active');
+      headerLogoBlack.classList.remove('active');
+      headerBtn.classList.remove('active');
+
+     }
+});
+
+// NAVIGATION SOUS JQUERY
+
+$(function() {
 
   $('.menu').click(function(e) {
     $('.nav-mobile').slideDown();
@@ -16,6 +48,26 @@ $(function() {
     end : function() {
       $('.nav-mobile').slideUp();
     }
-  });
+  })
 
-});  
+// SLIDER
+
+$('.slider-container').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  arrows: false,
+  infinite: true,  
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    }
+  ]
+});
+});
